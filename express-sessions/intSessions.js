06969def518842e34e1599d556a790bf4ad69140
rosewasp <55768481+rosewasp@users.noreply.github.com>
2020-05-17 18:45:46 -0700

    Update intSessions.js

--- express-sessions/intSessions.js
+++ express-sessions/intSessions.js
@@ -52,15 +52,16 @@ app.post('/',function(req,res){
     })
   }
 
-  if(req.body["End Session"]){
-    req.session.destroy();
-  };
-
   context.name = req.session.name;
   context.toDoCount = req.session.toDo.length;
   context.toDo = req.session.toDo;
   console.log(context.toDo);
   res.render('toDo',context);
+
+  if(req.body["End Session"]){
+    req.session.destroy();
+  };
+  
 });
 
 app.use(function(req,res){
