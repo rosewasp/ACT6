1645a5eaabfa4b6713d1321d8d2df9870bf73db2
rosewasp <55768481+rosewasp@users.noreply.github.com>
2020-05-17 18:47:57 -0700

    Update intSessions.js

--- express-sessions/intSessions.js
+++ express-sessions/intSessions.js
@@ -59,9 +59,26 @@ app.post('/',function(req,res){
   res.render('toDo',context);
 
   if(req.body["End Session"]){
+    req.session.toDo = []
     req.session.destroy();
+
+    app.use(function(req,res){
+      res.status(404);
+      res.render('404');
+    });
+    
+    app.use(function(err, req, res, next){
+      console.error(err.stack);
+      res.type('plain/text');
+      res.status(500);
+      res.render('500');
+    });
+    
+    app.listen(app.get('port'), function(){
+      console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+    });
   };
-  
+
 });
 
 app.use(function(req,res){
